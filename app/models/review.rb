50c641d256703fd78941ec467780e9f81da290f1
mlevine1708 <micah.levine@outlook.com>
2020-04-11 14:06:27 -0400

    completed review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,17 @@
 class Review < ApplicationRecord
   belongs_to :books
+  belongs_to :user
+
+
+  def book_title=(title)
+    self.book = Book.find_or_create_by(title: title)
+  end
+
+  def book_title
+    self.book ? self.book.title : nil
+  end
+
+  def user_name=(name)
+    self.user = User.find_or_create_by(name: name)
+  end
 end
