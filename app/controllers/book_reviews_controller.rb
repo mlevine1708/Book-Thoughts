a11d20b2ecddbddff0e39c81f2cd87c611c4d88e
mlevine1708 <micah.levine@outlook.com>
2020-04-13 13:52:50 -0400

    working on book_reviews

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -1,2 +1,20 @@
 class BookReviewsController < ApplicationController
-end
+
+    def new
+        if params[:book_id] && book = Book.find_by_id(params[:book_id])
+            @book = book.book_reviews.build
+        else
+            @book_review = Book_review.new
+            @book_review.build_book
+        end
+    end
+
+    def create
+        @book_review = current_user.book_reviews.build(book_review_params)
+        if @book_review.save
+            redirect_to book_path(@book_review)
+        else
+            @book_review.build_book unless @book_review.book
+            render :new
+        end
+    end
