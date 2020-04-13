567ffdc4b4157b9a9fd1746ed86c553405584ab5
mlevine1708 <micah.levine@outlook.com>
2020-04-12 21:09:50 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_secure_password 
+    validates :name, :email, presence: true
+    validates :name, :email, uniqueness: true
+
     has_many :reviews
     has_many :books, through: :reviews
 end
