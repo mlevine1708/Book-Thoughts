026b67a2169de4679520a37290e57964730dfd90
mlevine1708 <micah.levine@outlook.com>
2020-04-12 21:35:45 -0400

    added books controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
 
     #loading the signup form
-def new
-    @user = User.new 
-end
-
-#signup
-def create
-end
-
-
-
-
-
-end
+    def new
+      @user = User.new
+    end
+  
+    #signup
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        #login the user
+        session[:user_id] = @user.id
+        redirect_to books_path
+      else
+        render :new
+      end
+    end
+  
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+  
+  end
