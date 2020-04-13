5d73608c40c4d97dc83fce0230ab8c23f65ab90a
mlevine1708 <micah.levine@outlook.com>
2020-04-12 22:08:36 -0400

    fixed login route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,13 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: params[:user][:email])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to books_path
+        else
+            redirect_to :login 
+        end
     end
 
     def home
