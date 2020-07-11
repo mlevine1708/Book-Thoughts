7a5656821eacb5f294302434b3132c177b4443e4
micah.levine <github email address>
2020-07-11 16:39:19 -0400

    added current user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
     include ApplicationHelper
 
+
+    helpers do
+      def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
+      end
+
+
     private
         def check_for_logged_in
             redirect_to '/' if !logged_in?
         end
-        
-    
+
+
 end
