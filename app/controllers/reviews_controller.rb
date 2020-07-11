51ee6243c508936a4eab433a9e067295b4873c9c
micah.levine <github email address>
2020-07-11 16:33:23 -0400

    added create method to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,16 @@ class ReviewsController < ApplicationController
 
     render :new
 
+
+    def create
+      @review = current_user.reviews.build(review_params)
+      if @review.save
+        redirect_to review_path(@review)
+      else
+        @review.build_book unless @review.book
+        render :new
+      end
+    end 
+
+
 end
